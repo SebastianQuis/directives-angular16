7562c6b512b398b02959d7a7e6860e5048e3fc64
SebastianQuis <sebastianglg64@gmail.com>
2026-07-28 13:50:24 -0500

    culminando con las directivas en angular

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -7,31 +8,61 @@ export class CustomLabelDirective implements OnInit {
 
   private htmlElement?: ElementRef<HTMLElement>;
   public colorStyle: string = 'red';
+  private _errors?: ValidationErrors | null;
 
 
   @Input() set color(value: string) {
-    // console.log("value: ", value);
     this.colorStyle = value;
     this.setStyle(); // actualizar al enviar en el componente padre
   }
 
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage(); // actualizar al enviar en el componente padre
+  }
+
+
   constructor(
     private element: ElementRef<HTMLElement>,
   ) {
-    // console.log("element: ", element);
     this.htmlElement = element;
     this.htmlElement.nativeElement.innerHTML = 'El pepe :)'
   }
 
   ngOnInit(): void {
     console.log("customLabelDirective -> ngOnInit")
-    // cambiar el color del label al iniciar el componente
     this.setStyle();
   }
 
   setStyle(): void {
     if (this.htmlElement) {
       this.htmlElement.nativeElement.style.color = this.colorStyle;
+    }
+  }
+
+  setErrorMessage(): void {
+    if (!this.htmlElement) return;
+    if (!this._errors) {
+      this.htmlElement.nativeElement.innerHTML = '';
+      return;
+    }
+
+    const errorsArray = Object.keys(this._errors);
+    if (errorsArray.includes('required')) {
+      this.htmlElement.nativeElement.innerHTML = 'Este campo es requerido';
+      return
+    }
+
+    if (errorsArray.includes('minlength')) {
+      const min = this._errors['minlength'].requiredLength;
+      const actual = this._errors['minlength'].actualLength;
+      this.htmlElement.nativeElement.innerHTML = `Este campo debe tener al menos ${min} caracteres, actualmente tiene ${actual}`;
+      return
+    }
+
+    if (errorsArray.includes('email')) {
+      this.htmlElement.nativeElement.innerHTML = 'Este campo debe ser un correo electrónico válido';
+      return
     }
   }
 
